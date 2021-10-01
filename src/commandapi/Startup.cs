62574df279876e90e84efc3a7699182305c5d20b
mohit <mohitreshib@gmail.com>
2021-10-01 16:59:14 +0530

    added Interface

--- src/commandapi/Startup.cs
+++ src/commandapi/Startup.cs
@@ -18,7 +18,7 @@ namespace commandapi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
+            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
